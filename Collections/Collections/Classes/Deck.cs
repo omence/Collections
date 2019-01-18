0a6d76869f78fe27992cc6fe860a68af841bcd09
Jason Few <omence11@gmail.com>
2019-01-18 14:51:11 -0800

    Commented missing method comments, and added more to README.

--- Collections/Collections/Classes/Deck.cs
+++ Collections/Collections/Classes/Deck.cs
@@ -15,6 +15,11 @@ namespace Collections.Classes
         public T[] internalItems = new T[10];
         public int currentIndex = 0;
 
+
+        /// <summary>
+        /// This method adds items to list and resizes if need be
+        /// </summary>
+        /// <param name="item"></param>
         public void Add(T item)
         {
             if (currentIndex > internalItems.Length - 1)
@@ -57,7 +62,10 @@ namespace Collections.Classes
             }
 
         }
-
+        /// <summary>
+        /// Calls method from above
+        /// </summary>
+        /// <returns></returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
--- README.md
+++ README.md
@@ -6,8 +6,11 @@ The purpose of the app create playing cards, put them in a deck and deal them.
 
 How do I run the program?
 
-Click the run button
-Then choose the option to deal, show all cards or remove a card.
+Click the run button and you will see a user interface.  Make a selection, 1, 2, 3, or 4
+The options are to deal, show all cards or remove a card.
+The deal option will deal to 2 players.
+The Show all cards option will show all cards in the deck
+The remove option will remove a card from the deck. 
 
 What does the program look like? (visual)
 ![image](https://github.com/omence/Collections/blob/master/Screenshot%20(8).png)
